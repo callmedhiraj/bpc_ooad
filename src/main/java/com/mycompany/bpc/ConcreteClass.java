2dcb0d48ad972c5e8423b4c6c1ba053b7eb80dd6
dhiraj jha <callmedhiraj@gmail.com>
2024-09-03 21:38:54 +0545

    Day 3 class object inheritance abstract classes demo

---
+++ src/main/java/com/mycompany/bpc/ConcreteClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bpc;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ConcreteClass extends Pedagogy {
+    @Override
+    public void teachingMethod(){
+        System.out.println("Reverse Psychology");
+        
+    }
+    //own personal method
+    public void secretTeacher(){
+        System.out.println("I am a secret teacher teaching livlihood with books.");
+    }
+    
+}
---
+++ src/main/java/com/mycompany/bpc/TestPedagogy.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bpc;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class TestPedagogy {
+    public static void main(String[] args) {
+        //creating an object of abstract class, illegal
+        ConcreteClass p = new ConcreteClass();
+        p.teachingMethod();
+}
+}
